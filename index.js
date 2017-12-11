ffe8fa4f5a25d96fbf259e12956d986eb9c21fa3
Sofya Verba <sofyaverba@gmail.com>
2017-12-11 22:05:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function yourAnimal() {
 
 function add2() {
   return n + two
-  function n() {
+
   // Feel free to move things around!
-  const two = n + two
+  const two = 2
 }
 
 var funkyFunction = function() {
